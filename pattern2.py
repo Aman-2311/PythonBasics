a9143a539618ee2d7aab06d3f6ce44f905dd4dc2
Amangithubb <amansingh42586@gmail.com>
2025-06-25 13:44:52 +0530

    heart shap

--- pattern2.py
+++ pattern2.py
@@ -1,4 +1,21 @@
-rows = 5  # You can change this value for a bigger or smaller triangle
+rows = int(input("Enter the number of rows: "))
 
+for i in range(rows // 2, rows, 2):
+    # top left curve
+    for j in range(1, rows - i, 2):
+        print(" ", end="")
+    for j in range(1, i + 1):
+        print("*", end="")
+    for j in range(1, rows - i + 1):
+        print(" ", end="")
+    for j in range(1, i + 1):
+        print("*", end="")
+    print()
+
+# lower part
 for i in range(rows, 0, -1):
-    print('*' * i)
+    for j in range(rows - i):
+        print(" ", end="")
+    for j in range(1, i * 2):
+        print("*", end="")
+    print()
