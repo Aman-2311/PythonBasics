5554ba5249b645952cd4fb8ca5330c51b3f3a9d7
Amangithubb <amansingh42586@gmail.com>
2025-06-28 23:21:28 +0530

    print N to 1 numbers

--- recursive.py
+++ recursive.py
@@ -7,3 +7,16 @@ def sum_natural(n):
 # Calling the function
 n = 5
 print("Sum of first", n, "natural numbers:", sum_natural(n))
+
+
+#print N to 1
+
+def print_n_to_1(n):
+    if n == 0:
+        return
+    print(n)
+    print_n_to_1(n - 1)
+
+# Example:
+print_n_to_1(5)
+# Output: 5 4 3 2 1
