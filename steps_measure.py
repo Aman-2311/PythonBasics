3017a54c6e10e474bdbd99a23d73a9f6d9da9a3a
Amangithubb <amansingh42586@gmail.com>
2025-07-03 23:28:46 +0530

    Random step measure

---
+++ steps_measure.py
@@ -0,0 +1,11 @@
+#steps
+import numpy as np
+user_steps = int(input("Enter the number of steps: "))
+steps = np.random.choice([1,-1],size =user_steps)
+position = np.cumsum(steps)
+
+position = np.insert(position,0,0)
+
+print(".../Random walk path")
+print(position)
+print(f"\n Final position after{user_steps} steps :{position[-1]}")
