1320bf61b7d0343ed54a9b5b596e9962a8bd49a2
Aman07 <135688213+Aman-2311@users.noreply.github.com>
2025-07-16 23:40:40 +0530

    Update temp_conv.py

--- temp_conv.py
+++ temp_conv.py
@@ -2,7 +2,7 @@ print("Temperature Converter")
 print("1.Celcius to Fahrenheit")
 print("2.Fahrenheit to Celcius")
 
-choice = int(input("Enter your choice:"))
+choice = int(input("Enter your choice :- "))
 if choice == 1:
   celcius =float(input("Enter the temperature in celcius:"))
   fahrenheit =(celcius*9/5)+32 
